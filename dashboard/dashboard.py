1227835121930c56a274996addb6c43d8f0afd93
Lusi Aulia Jati <99398744+lusiaulia@users.noreply.github.com>
2024-01-31 01:43:20 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,8 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-import babel
-from babel.numbers import format_currency
 
 sns.set(style='white')
 
